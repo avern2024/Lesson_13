d60c8e5439f44452e108a8100076658cc99b235b
avern2024 <avern2020@icloud.com>
2024-12-26 19:07:51 +0700

    Add files via upload

---
+++ module_13_6.py
@@ -0,0 +1,109 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+ap_token = ""
+bot = Bot(token=ap_token)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button, button2)
+
+kb_inline = InlineKeyboardMarkup()
+button_inline = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='calories')
+button_inline2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+kb_inline.add(button_inline, button_inline2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+async def validate_numeric_input(message: types.Message):
+    if not message.text.isdigit():
+        await message.answer("Пожалуйста, введите число.")
+        return None
+    value = int(message.text)
+
+    return value
+
+
+@dp.message_handler(commands=['start'])
+async def start_command(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью', reply_markup=kb)
+
+
+@dp.message_handler(text='Информация')
+async def info(message: types.Message):
+    await message.answer('AvernBot, version 1.0')
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message: types.Message):
+    await message.answer('Выберите опцию:', reply_markup=kb_inline)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('10 x вес(кг) + 6.25 x рост(см) - 5 x возраст(г) - 161')
+    await call.answer()
+
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    age = await validate_numeric_input(message)
+    if age is None:
+        return  # Если ввод некорректен, завершить обработку
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    growth = await validate_numeric_input(message)
+    if growth is None:
+        return
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    weight = await validate_numeric_input(message)
+    if weight is None:
+        return
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+
+    # Упрощенная формула для расчета нормы калорий (для женщин)
+    calories = 10 * weight + 6.25 * growth - 5 * age - 161
+
+    await message.answer(f'Ваша норма калорий: {calories:.2f}')
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_messages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
